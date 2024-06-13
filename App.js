a7befbd4282e71acda1e276f056874f72c7e40e4
unknown <athuldinesh@gmail.com>
2024-06-13 18:33:09 -0400

    A5: Assignment changes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Lab5 from './Lab5/index.js';
 import cors from "cors";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
 const app = express();
 app.use(cors());
@@ -13,5 +14,6 @@ Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 
 app.listen(process.env.PORT || 4000);
